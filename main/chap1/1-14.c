a7305667bf6ffabfc6420a11f3323e9267ff9e18
Theodor Blom <me@theodorblom.com>
2024-02-26 17:21:05 +0100

    Adding exercise 1-14

---
+++ main/chap1/1-14.c
@@ -0,0 +1,46 @@
+/*
+Exercise 1-13.
+Write a program to print a histogram of the lengths or words in its input. It is
+easy to draw the histogram with the bars horizontal; a vertical orientation is
+more challenging.
+*/
+
+#include <ctype.h>
+#include <stdio.h>
+
+#define START_CHAR 'a'
+#define END_CHAR 'z'
+#define NUM_CHAR END_CHAR - START_CHAR + 1
+
+int main()
+{
+    int c;
+    int c_hist[NUM_CHAR];
+
+    for (int i = 0; i < NUM_CHAR; i++)
+    {
+        c_hist[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF)
+    {
+        c = tolower(c); /* To handle both upper- and lowercase characters */
+        if (c >= START_CHAR && c <= END_CHAR)
+        {
+            ++c_hist[c - START_CHAR];
+        }
+    }
+
+    printf("CHARACTER OCCURANCE HISTOGRAM:\n");
+    for (int i = 0; i < NUM_CHAR; i++)
+    {
+        printf("%c|", START_CHAR + i);
+        for (int j = 0; j < c_hist[i]; j++)
+        {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
