fe3da2db556c08b7c24a3e5d29e1556ea8896271
Theodor Blom <me@theodorblom.com>
2024-03-08 16:32:33 +0100

    Printing scientific notation for floats

--- main/chap2/2-1.c
+++ main/chap2/2-1.c
@@ -16,9 +16,9 @@ int main()
     printf("== Short ==\nUnsigned:\t[%2i, %i]\nSigned:\t\t[%i, %i]\n\n", 0, USHRT_MAX, SHRT_MIN, SHRT_MAX);
     printf("== Int ==\nUnsigned:\t[%2i, %u]\nSigned:\t\t[%i, %i]\n\n", 0, UINT_MAX, INT_MIN, INT_MAX);
     printf("== Long ==\nUnsigned:\t[%2i, %lu]\nSigned:\t\t[%li, %li]\n\n", 0, ULONG_MAX, LONG_MIN, LONG_MAX);
-    printf("== Float ==\nUnsigned:\t[%f, %f]\n\n", FLT_MIN, FLT_MAX);
-    printf("== Double ==\nUnsigned:\t[%lf, %lf]\n\n", DBL_MIN, DBL_MAX);
-    printf("== Long Double ==\nUnsigned:\t[%Lf, %Lf]\n\n", LDBL_MIN, LDBL_MAX);
+    printf("== Float ==\nUnsigned:\t[1e%i, 1e%i]\n\n", FLT_MIN_10_EXP, FLT_MAX_10_EXP);
+    printf("== Double ==\nUnsigned:\t[1e%i, 1e%i]\n\n", DBL_MIN_10_EXP, DBL_MAX_10_EXP);
+    printf("== Long Double ==\nUnsigned:\t[1e%i, 1e%i]\n\n", LDBL_MIN_10_EXP, LDBL_MAX_10_EXP);
 
     return 0;
 }
